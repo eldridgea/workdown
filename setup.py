1ccc5bd59f1f2b2f0ab2fde46b27e52d6771f7b4
Eldridge Alexander <ealexander@duosecurity.com>
2019-09-22 21:48:20 -0400

    cli

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/eldridgea/workdown",
     packages=setuptools.find_packages(),
+    install_requires=['markdown'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
