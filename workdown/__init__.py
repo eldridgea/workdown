7d14205ba1ab0a7605ab5591f9cac5bfe8f04c37
Eldridge Alexander <ealexander@duosecurity.com>
2019-09-22 21:09:10 -0400

    packaging

---
+++ workdown/__init__.py
@@ -0,0 +1,12 @@
+"""
+    workdown
+    ~~~~~~~~~~~~~~
+
+    workdown - Write markdown and host pages on Cloudflare Workers
+
+    :copyright: (c) 2019 by ELdridge Alexander.
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+__version__ = '1.4.3'
