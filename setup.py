574efe9ce0849319d1b1f883573d611a480a70b2
Eldridge Alexander <ealexander@duosecurity.com>
2019-09-22 21:12:38 -0400

    cli

--- setup.py
+++ setup.py
@@ -19,4 +19,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+        entry_points={
+        'console_scripts': [
+            'workdown = workdown:main'
+        ]
+    },
 )
