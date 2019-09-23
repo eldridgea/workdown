fd86271bb4089e0705926c6a242b256c2f80e4f1
Eldridge Alexander <ealexander@duosecurity.com>
2019-09-23 19:07:24 -0400

    added publishing of script itself

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="workdown",
-    version="0.0.3",
+    version="0.0.4",
     author="Eldridge Alexander",
     author_email="eldridgea@gmail.com",
     description="Write Markdown and have it published and hosted on Cloudflare Workers",
