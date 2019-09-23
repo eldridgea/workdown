8075165cea4b04afdeff2f64d8d65d096be85d0b
Eldridge Alexander <ealexander@duosecurity.com>
2019-09-22 20:44:53 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="workdown",
+    version="0.0.1",
+    author="Eldridge Alexander",
+    author_email="eldridgea@gmail.com",
+    description="Write Markdown and have it published and hosted on Cloudflare Workers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eldridgea/workdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
